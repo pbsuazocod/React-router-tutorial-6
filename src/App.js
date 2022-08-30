186c9c8358fb1ab601a9f6f9f7f94cd9881eae46
unknown <pbsuazocod@gmail.com>
2022-08-30 18:14:38 -0400

    general

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+  </Routes>
+  </BrowserRouter>);
 }
 
 export default App;
